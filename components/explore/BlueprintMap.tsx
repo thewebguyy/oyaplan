1f924420a370aaed542148d6347c492ab37e88fd
Olabode Olusegun <olabodewebdesigns02@gmail.com>
2026-07-13 09:57:35 +0100

    refactor(explore): extract AREAS config into central constant

--- components/explore/BlueprintMap.tsx
+++ components/explore/BlueprintMap.tsx
@@ -2,64 +2,7 @@
 
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
-const AREAS = [
-  {
-    name: "Ikeja",
-    slug: "ikeja",
-    points: "80,60 260,60 260,180 80,180",
-    textX: 170,
-    textY: 120,
-    neonColor: "#6B0F1A" // Deep Oxblood/Maroon
-  },
-  {
-    name: "Surulere",
-    slug: "surulere",
-    points: "60,200 180,200 180,310 60,310",
-    textX: 120,
-    textY: 255,
-    neonColor: "#5A00FF" // Ultra Violet
-  },
-  {
-    name: "Yaba",
-    slug: "yaba",
-    points: "200,200 320,200 320,310 200,310",
-    textX: 260,
-    textY: 255,
-    neonColor: "#FF1493" // Vivid Pink
-  },
-  {
-    name: "Lagos Island",
-    slug: "lagos-island",
-    points: "340,230 460,230 440,320 320,320",
-    textX: 390,
-    textY: 275,
-    neonColor: "#E0115F" // Bright Fuchsia
-  },
-  {
-    name: "Ikoyi",
-    slug: "ikoyi",
-    points: "480,210 620,210 600,310 460,310",
-    textX: 540,
-    textY: 260,
-    neonColor: "#FF00FF" // Hot Magenta
-  },
-  {
-    name: "Victoria Island",
-    slug: "vi",
-    points: "360,330 580,330 560,440 340,440",
-    textX: 460,
-    textY: 385,
-    neonColor: "#0047FF" // Electric Cobalt
-  },
-  {
-    name: "Lekki",
-    slug: "lekki-phase-1",
-    points: "600,330 760,330 740,440 580,440",
-    textX: 670,
-    textY: 385,
-    neonColor: "#00FFFF" // Neon Cyan
-  }
-];
+import { AREAS } from "@/lib/config/areas";
 
 export default function BlueprintMap() {
   const router = useRouter();
